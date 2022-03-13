19fee5b0dcaddb9d66a974e54e53c1c8b6114ee1
Christopher Leonard <chris.j.leonard@gmx.com>
2022-03-13 22:57:16 +0000

    Progress

--- sf-editor.fs
+++ sf-editor.fs
@@ -1,14 +1,21 @@
 WORDLIST CONSTANT EDITOR-WL
 : EDITOR  GET-ORDER NIP EDITOR-WL SWAP SET-ORDER ;
-ALSO EDITOR  EDITOR-WL SET-CURRENT
-SYNONYM FORTH FORTH
+ALSO EDITOR DEFINITIONS
+: FORTH  FORTH ;
 
 VARIABLE CHR
+CREATE 'FND 64 ALLOT  VARIABLE #FND
+CREATE 'INS 64 ALLOT  VARIABLE #INS
 
 : -ROT ( x1 x2 x3 - x3 x1 x2)  ROT ROT ;
 : HUH ( a n)  TYPE  [CHAR] ? EMIT  ABORT ;
 : CHOOSE ( ? x1 x2 - x1|x2)  ROT IF SWAP THEN NIP ;
-: TIB@ ( a)  SOURCE DROP ;  : #TIB ( n)  SOURCE NIP ;
+
+[UNDEFINED] TIB [UNDEFINED] #TIB OR [IF]
+: TIB ( a)  SOURCE DROP ;  : #TIB@ ( n)  SOURCE NIP ;
+[ELSE]
+DUP 0= ?END  : #TIB@ ( - a)  #TIB @ ;
+[THEN]
 
 : N  1 SCR +! ;
 : B  -1 SCR +! ;
@@ -17,28 +24,29 @@ VARIABLE CHR
 : LIN ( - n)  CHR @ 6 RSHIFT  15 MIN ;
 : ^LIN ( - n)  LIN 6 LSHIFT ;
 : LIN^ ( - n)  LIN 1+ 6 LSHIFT ;
-: PRI? ( c - ?)  32 128 WITHIN ;
-: SCR@ ( n - c)  SCR @ BLOCK + C@ ;
-: PRI ( n1 n2)  ?DO I SCR@ DUP PRI? SWAP BL CHOOSE LOOP ;
+: PRI? ( c - ?)  32 127 WITHIN ;
+: SCR+ ( n - c)  SCR @ BLOCK + ;
+: LINE ( n - a)  6 LSHIFT SCR+ ;
+: PRI ( n1 n2)  ?DO I SCR+ @ DUP PRI? SWAP BL CHOOSE LOOP ;
 : TYP  CR  ^LIN CHR @ PRI  [CHAR] ^ EMIT  CHR @ LIN^ PRI
        SPACE LIN . ;
 : T ( n)  6 LSHIFT  CHR !  TYP ;
-: TRAIL ( - a n)  CHR @ SCR@  LIN^ CHR @ - ;
+: TRAIL ( - a n)  CHR @ SCR+  LIN^ CHR @ - ;
 : -FOUND  'FND #FND @ HUH ;
-: FND ( a1 n)  TRAIL 2SWAP SEARCH NIP
-               IF ( FIXME -- start of string is wrong, should be end) 0 SCR@ - CHR !  ELSE -FOUND THEN ;
+: FND ( a n)  TRAIL 2SWAP SEARCH NIP
+              IF 0 SCR+ - CHR !  ELSE -FOUND THEN ; ( FIXME -- start of string is wrong, should be end)
 : BLANK ( a n)  0 ?DO BL DUP I + C! LOOP 2DROP ;
-: END  #TIB >IN ! ;
-: END?  #TIB >IN @ = ;
-: >>> ( - a n ?)  TIB@ >IN @ +  #TIB >IN @ -  END? 0=  END ;
+: END  #TIB@ >IN ! ;
+: END? ( - ?)  #TIB@ >IN @ = ;
+: >>> ( - a n ?)  TIB >IN @ +  #TIB@ >IN @ -  END? 0=  END ;
 : >FND'  >>> IF DUP #FND ! 'FND SWAP MOVE ELSE 2DROP THEN ;
 : >INS'  >>> IF DUP #INS ! 'INS SWAP MOVE ELSE 2DROP THEN ;
-: >FND ( - a n)  >FND'  'FND #FND @ ;
-: >INS ( - a n)  >INS'  'INS #INS @ ;
+: >FND ( - a n)  >FND'  'FND #FND @  CR ;
+: >INS ( - a n)  >INS'  'INS #INS @  CR ;
 : F  >FND FND ;
-: R  E I ;
 : I'  >INS INS ;
 : I  I' TYP ;
-: P  ^LIN DUP CHR !  SCR@ 64 BLANK  UPDATE  I' ;
+: R  E I ;
+: P  ^LIN DUP CHR !  SCR+ 64 BLANK  UPDATE  I' ;
 
-PREVIOUS  FORTH-WORDLIST SET-CURRENT
+PREVIOUS DEFINITIONS
